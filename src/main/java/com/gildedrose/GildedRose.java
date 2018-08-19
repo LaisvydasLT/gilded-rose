466ad083cf93079456ecd5744d74e823c174a884
Laisvydas <laisviz@gmail.com>
2018-08-19 20:34:37 +0300

    More refactoring.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,52 +1,80 @@
 package com.gildedrose;
 
 class GildedRose {
-	
+
 	private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
-    private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
-    private static final String AGED_BRIE = "Aged Brie";
-	
-    Item[] items;
+	private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+	private static final String AGED_BRIE = "Aged Brie";
 
-    public GildedRose(Item[] items) {
-        this.items = items;
-    }
+	private static final int MAX_QUALITY = 50;
+	private static final int MIN_QUALITY = 0;
 
-    public void updateQuality() {
-    	for (Item item : items) {
-            if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE_PASSES)) {
-                if (item.quality > 0 && !item.name.equals(SULFURAS)) {
-                	item.quality--;
-                }
-            } else if (item.quality < 50) {
-                    item.quality++;
+	Item[] items;
 
-                    if (item.name.equals(BACKSTAGE_PASSES) && item.quality < 50) {
-                    	if (item.sellIn < 6) {
-                        	item.quality += 2;
-                        } else if (item.sellIn < 11) {
-                        	item.quality++;
-                        }
-                    }
-            }
+	public GildedRose(Item[] items) {
+		this.items = items;
+	}
 
-            if (!item.name.equals(SULFURAS)) {
-                item.sellIn--;
-            }
+	public void updateQuality() {
+		for (Item item : items) {
+			switch (item.name) {
+			case AGED_BRIE: {
+				updateAgedBrie(item);
+				break;
+			}
+			case BACKSTAGE_PASSES: {
+				updateBackstagePasses(item);
+				break;
+			}
+			case SULFURAS: {
+				break;
+			}
+			default: {
+				updateOtherItem(item);
+			}
+			}
+		}
+	}
 
-            if (item.sellIn < 0) {
-                if (!item.name.equals(AGED_BRIE)) {
-                    if (!item.name.equals(BACKSTAGE_PASSES)) {
-                        if (item.quality > 0 && !item.name.equals(SULFURAS)) {
-                        	item.quality--;
-                        }
-                    } else {
-                        item.quality = 0;
-                    }
-                } else if (item.quality < 50) {
-                        item.quality++;
-                }
-            }
-        }
-    }
+	private void updateAgedBrie(Item item) {
+		increaseQuality(item);
+		item.sellIn--;
+		if (item.sellIn < 0) {
+			increaseQuality(item);
+		}
+	}
+
+	private void updateBackstagePasses(Item item) {
+		increaseQuality(item);
+		if (item.sellIn < 11) {
+			increaseQuality(item);
+		}
+		if (item.sellIn < 6) {
+			increaseQuality(item);
+		}
+		item.sellIn--;
+		if (item.sellIn < 0) {
+			item.quality = 0;
+		}
+	}
+
+	private void updateOtherItem(Item item) {
+		decreaseQuality(item);
+		item.sellIn--;
+		if (item.sellIn < 0) {
+			decreaseQuality(item);
+		}
+	}
+
+	public void increaseQuality(Item item) {
+		if (item.quality < MAX_QUALITY) {
+			item.quality++;
+		}
+	}
+
+	public void decreaseQuality(Item item) {
+		if (item.quality > MIN_QUALITY) {
+			item.quality--;
+		}
+	}
 }
